c1ab940d0f21a3c4a298141320f7b29d288d9a0b
Oliver Klee <typo3-coding@oliverklee.de>
2013-10-10 16:30:08 +0200

    [TASK] Create the initial Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gethdo::Application.config.secret_key_base = '2bc8dd3cf8178571a6e45dd2185715450b17ccc7a1631e0202b1587dfa56967829d4d4437482722612167c72ed6aa68e893ffa3ae2d71a79408547b79a59c59f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gethdo::Application.config.session_store :cookie_store, key: '_gethdo_session'
