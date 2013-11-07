797dd13c0ca5eed963d989f7b59bbca44ea8f10b
Oliver Klee <typo3-coding@oliverklee.de>
2013-11-07 21:05:17 +0100

    [CLEANUP] Drop the unused Users fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107193705) do
+ActiveRecord::Schema.define(version: 20131107195341) do
 
   create_table "users", force: true do |t|
-    t.integer  "gender"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email"
-    t.boolean  "email_is_validated"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
