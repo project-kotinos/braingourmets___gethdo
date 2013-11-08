8938284f03a6686d515b9bb8dbd403c1da1c1960
Oliver Klee <typo3-coding@oliverklee.de>
2013-11-08 14:16:13 +0100

    [FEATURE] Add devise to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# This model represents a user, i.e., a person that can log in.
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
