33027a39a04ed3f23bb481aac6a77971e84f15c6
Oliver Klee <typo3-coding@oliverklee.de>
2013-12-12 16:39:55 +0100

    [TASK] Enable i18n warnings to avoid the deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Gethdo
   class Application < Rails::Application
+    # Enable i18n warnings to avoid the deprecation warning.
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
