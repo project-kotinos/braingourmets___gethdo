09cfe862d30d92fad567039ce57bfbb5083c85ea
Oliver Klee <typo3-coding@oliverklee.de>
2013-12-27 23:28:49 +0100

    [BUGFIX] Move the secret key base to the local configuration.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Gethdo::Application.config.secret_key_base = '2bc8dd3cf8178571a6e45dd2185715450b17ccc7a1631e0202b1587dfa56967829d4d4437482722612167c72ed6aa68e893ffa3ae2d71a79408547b79a59c59f'
+Gethdo::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
