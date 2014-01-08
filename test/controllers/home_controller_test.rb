309cfa5ed4baf1495f6b90babf4b52f4e9433ee8
Oliver Klee <typo3-coding@oliverklee.de>
2014-01-08 19:13:30 +0100

    [FEATURE] Add some functional tests.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 # Test case.
+
 class HomeControllerTest < ActionController::TestCase
-  test 'should get index' do
+  test 'index gets home/index with application layout' do
     get :index
     assert_response :success
+
+    assert_template 'home/index'
+    assert_template layout: 'layouts/application'
+  end
+
+  test 'index has Gethdo title' do
+    get :index
+
+    assert_select 'title', 'Gethdo'
   end
 end
