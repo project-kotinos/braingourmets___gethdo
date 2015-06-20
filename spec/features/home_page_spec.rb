a6acb03bd5bc706cd86a5e265c28f2dfdfcf5813
Oliver Klee <o.klee@braingourmets.com>
2015-06-20 13:16:36 +0200

    [TASK] Update the RSpec configuration
    Now the RSpec setup matches the setup recommended by the latest rspec-rails
    version.
    
    Closes #101
    Signed-off-by: Michael Schulze <m.schulze@braingourmets.com>

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'rails_helper'
 
-feature 'the home page' do
+RSpec.feature 'the home page', type: :feature do
   scenario 'has "Getho" as HTML title' do
     visit '/'
     expect(page).to have_title('Gethdo')
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe User do
+RSpec.describe User, type: :model do
   let(:user) { create(:user) }
 
   it 'is valid with valid attributes' do
