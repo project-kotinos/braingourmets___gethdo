e3f4420fc1f06e8b65468c3530ae96925adba286
Oliver Klee <github@oliverklee.de>
2017-01-29 14:01:42 +0100

    [TASK] Use Rails-style indentation settings for RuboCop (#140)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,30 +12,31 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  ##
-  # Sets the locale from the parameters using the default locale as a fallback.
-  #
-  def set_i18n_local_from_parameters
-    locale = params[:locale]
-    return false unless locale
+    ##
+    # Sets the locale from the parameters using the default locale as a
+    # fallback.
+    #
+    def set_i18n_local_from_parameters
+      locale = params[:locale]
+      return false unless locale
 
-    if I18n.locale_available? locale
-      I18n.locale = locale
-    else
-      show_and_log_flash_notice("#{locale} translation not available")
-      I18n.locale = I18n.default_locale
+      if I18n.locale_available? locale
+        I18n.locale = locale
+      else
+        show_and_log_flash_notice("#{locale} translation not available")
+        I18n.locale = I18n.default_locale
+      end
     end
-  end
 
   private
 
-  ##
-  # Shows a message as a flash notice and logs it as an error.
-  #
-  # @param [String] message_text the message to show and log
-  #
-  def show_and_log_flash_notice(message_text)
-    flash.now[:notice] = message_text
-    logger.error message_text
-  end
+    ##
+    # Shows a message as a flash notice and logs it as an error.
+    #
+    # @param [String] message_text the message to show and log
+    #
+    def show_and_log_flash_notice(message_text)
+      flash.now[:notice] = message_text
+      logger.error message_text
+    end
 end
