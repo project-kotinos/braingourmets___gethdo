ad6cfc9e946a0b723f14d91bd413fe3dbe7a183c
Oliver Klee <github@oliverklee.de>
2018-10-09 15:51:16 +0200

    [TASK] Update all gems (#182)

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user, class: User do
-    email 'joe@example.com'
-    password 'EcCoc1Shnyn6'
+    email { 'joe@example.com' }
+    password { 'EcCoc1Shnyn6' }
   end
 end
